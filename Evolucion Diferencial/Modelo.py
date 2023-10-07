bfc634e1822c9abb3dbe95be4de37301c4fba7e6
EliasOros <86860663+EliasOros@users.noreply.github.com>
2023-10-07 14:11:00 -0600

    Evolucion Diferencial

---
+++ Evolucion Diferencial/Modelo.py
@@ -0,0 +1,43 @@
+
+###############################################################################################
+###############################           ATRIBUTOS          ##################################
+
+
+#Metodo para calcular el peso
+
+def peso():
+    
+    print ("Peso")
+    
+#Metodo para calcular si es minimo
+
+def minimo():
+    
+    print ("minimo")
+    
+#Metodo para calcular si es maximo
+
+def maximo ():
+    
+    print("maximo")
+    
+#Metodo llenado de poblacion inicial
+
+def poblacionInicial():
+    
+    print("llenado")
+    
+#Metodo llenado de generacion
+
+def poblecionGeneracion():
+    
+    print("Hola")
+    
+#Metodo eleccion de objetivo y vs
+
+def eleccionObjVs():
+    
+    print("Wuenas")
+    
+    
+    
---
+++ Evolucion Diferencial/Vista.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Evolucion Diferencial")
+
+label0 = tk.Label(root, text = ("Elige el tamaño de tu poblacion:"))
+label0.grid(row=0,column=0,padx=10, pady=10)
+
+opcionesPoblacion = ("4","5","6")
+
+spinbox_label0 = tk.Spinbox(root, values = opcionesPoblacion)
+spinbox_label0.grid(row=0,column=1, padx=10, pady=10)
+
+def crearPoblacion():
+    
+    print("hola")
+    
+    button_label0.config(state="disabled")
+    button_label1.config(state="active")
+    spinbox_label0.config(state="disabled")
+    spinbox_label1.config(state="normal")
+
+
+button_label0 = tk.Button(root, text =("Agregar"), command = crearPoblacion  )
+button_label0.grid(row=0,column=2)
+
+
+label1 = tk.Label(root, text = ("Cuantas generaciones quieres hacer:"))
+label1.grid(row=1,column=0,padx=10, pady=10)
+
+opcionesPoblacion = ("2","3")
+
+spinbox_label1 = tk.Spinbox(root, values = opcionesPoblacion, state="disabled")
+spinbox_label1.grid(row=1,column=1, padx=10, pady=10)
+
+def crearGeneracionesPoblacion():
+    
+    print("hola")
+    
+    button_label1.config(state="disabled")
+    button_reinicio.config(state="active")
+    spinbox_label1.config(state="disabled")
+
+
+button_label1 = tk.Button(root, text =("Calcular"), command = crearGeneracionesPoblacion, state="disabled"  )
+button_label1.grid(row=1,column=2)
+
+
+def reinciar():
+    
+    print("hola")
+    button_reinicio.config(state="disabled")
+    button_label0.config(state="active")
+    button_label1.config(state="active")
+    spinbox_label0.config(state="normal")
+    
+
+button_reinicio = tk.Button(root,text =("Reiniciar"), command = reinciar, state="disabled")
+button_reinicio.grid(row = 2, column=1)
+
+
+root.mainloop()
+
