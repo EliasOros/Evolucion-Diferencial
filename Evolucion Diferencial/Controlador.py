8ffe75cc074ff686fe2a53ea34fda99a20f84d28
CesarIRM19 <navirasec1.0@gmail.com>
2023-10-07 19:42:24 -0600

    Ya funciona pero sin hilos

--- Evolucion Diferencial/Controlador.py
+++ Evolucion Diferencial/Controlador.py
@@ -1,1 +1,15 @@
 #controlador
+from Modelo import poblacionInicial, poblacionGeneracion
+import Modelo
+PI = []
+def llamarCreacion(inicial):
+    global PI
+    PI = poblacionInicial(inicial)
+    
+
+def iniciarGeneraciones(gen):
+    valorgen = int(gen)
+    for i in range(valorgen):
+        for j in range (len(PI)):
+            poblacionGeneracion(PI,j)
+        print ("Esta es la ",i+1,"° generacion: ",Modelo.nueva_gen,". Peso de esta generacion: ",Modelo.pesoIn)
