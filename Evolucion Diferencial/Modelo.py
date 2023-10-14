c7f7c1e9483d3de125b5e74e91de151f038ea34d
EliasOros <86860663+EliasOros@users.noreply.github.com>
2023-10-13 21:54:33 -0600

    Update Modelo.py

--- Evolucion Diferencial/Modelo.py
+++ Evolucion Diferencial/Modelo.py
@@ -7,8 +7,53 @@ gen=[]
 pesoIn = []
 nueva_gen = []
 
-#Metodo para calcular el peso
+#random
+def generarRandom(PI, obj):
+    nlista = []
+    tamano=len(PI)
+    for i in range(tamano):
+        if i != obj:
+            nlista.append(PI[i])
+    return nlista
 
+#Metodo llenado de poblacion inicial
+
+def poblacionInicial(inicial):
+    global pesoIn
+    valorIn = int(inicial)
+    
+    PI = [ [    [] for _ in range(3)]    for _ in range(valorIn)]
+       
+    val = 0
+    
+    for i in range(len(PI)):
+        for j in range(3):
+            val = random.randint(-10, 10)
+            PI[i][j]= val
+    
+    print ("Este es la ponblacion inicial: ",PI)
+    for a in PI:
+        p = peso(a)
+        pesoIn.append(p)
+
+    print ("Este es el peso inicial: ",pesoIn,"\n")
+    return PI
+
+#Calculo de Wij = V1 + mu (v2 - v3)
+def calculoWij(list):
+    w=[]
+    op=[]
+    
+    for i in range(3):
+        #print(ord_lista[i][0])
+        for j in range(len(list)):
+            op.append(list[j][i])
+
+        w.append(op[0] + ((1/2)*(op[1] - op[2])))
+        op=[]
+    return w
+
+#Metodo para calcular el peso
 def peso(w):
     x = w[0]
     y = w[1]
@@ -16,12 +61,24 @@ def peso(w):
     
     p = pow(x,2) + pow(y,3) + pow(z,4)
     return p
-    
+       
 #Metodo para calcular si es minimo
-
-def minimo():
+def minimo(w,obj,PI):
     
-    print ("minimo")
+    pesoGen = peso(w)
+    pesoCom = pesoIn[obj]
+    print("Este es w: ",w)
+    print("Esto es el peso inicial: ",pesoCom)
+    print("Esto es el peso a comparar: ",pesoGen)
+    print("El objetivo: ", obj,"\n")
+    if(pesoGen < pesoCom):
+        pesoIn[obj] = pesoGen
+        nueva_gen.append(w)
+        print("Cambiamos por w \n")
+    else:
+        nueva_gen.append(PI[obj])
+        print("Dejamos el anterior \n")
+    
     
 #Metodo para calcular si es maximo
 
@@ -39,29 +96,8 @@ def maximo (w,obj,PI):
     else:
         nueva_gen.append(PI[obj])
         print("Dejamos el anterior \n")
-    
-#Metodo llenado de poblacion inicial
-
-def poblacionInicial(inicial):
-    global pesoIn
-    valorIn = int(inicial)
-    PI = [[[] for _ in range(3)] for _ in range(valorIn)]
-    val = 0
-    for i in range(len(PI)):
-        for j in range(3):
-            val = random.randint(-10, 10)
-            PI[i][j]= val
-    
-    print ("Este es la ponblacion inicial: ",PI)
-    for a in PI:
-        p = peso(a)
-        pesoIn.append(p)
-
-    print ("Este es el peso inicial: ",pesoIn,"\n")
-    return PI
-    
+        
 #Metodo llenado de generacion
-
 def poblacionGeneracion(PI, obj):
     
     nlista = generarRandom(PI, obj)
@@ -73,29 +109,12 @@ def poblacionGeneracion(PI, obj):
         x = gen[i]
         ord_lista.append(nlista[x])
     
-    w = operacio(ord_lista)
+    w = calculoWij(ord_lista)
     
     maximo(w,obj,PI)
     
-#Metodo eleccion de objetivo y vs
 
-def eleccionObjVs():
-    
-    print("Wuenas")
-
-
-
-def generarRandom(PI, obj):
-    nlista = []
-    tamano=len(PI)
-    for i in range(tamano):
-        if i != obj:
-            nlista.append(PI[i])
-    return nlista
-
-
-
-def operacio(list):
+def calculoWij(list):
     w=[]
     op=[]
     
@@ -114,7 +133,6 @@ def operacio(list):
 
 
 #Esto solo es para que funcione en consola
-
 """
 def main():
     global nueva_gen
