5a60bb162d82d9c912a46ff20642e5d8d5b62ad7
EliasOros <86860663+EliasOros@users.noreply.github.com>
2023-10-13 21:54:08 -0600

    Update Controlador.py

--- Evolucion Diferencial/Controlador.py
+++ Evolucion Diferencial/Controlador.py
@@ -1,11 +1,18 @@
 #controlador
 from Modelo import poblacionInicial, poblacionGeneracion
+##from VentanaEmergente import imprimir
 import Modelo
+
+
 PI = []
 def llamarCreacion(inicial):
     global PI
     PI = poblacionInicial(inicial)
     
+
+
+
+
 
 def iniciarGeneraciones(gen):
     valorgen = int(gen)
@@ -13,3 +20,5 @@ def iniciarGeneraciones(gen):
         for j in range (len(PI)):
             poblacionGeneracion(PI,j)
         print ("Esta es la ",i+1,"° generacion: ",Modelo.nueva_gen,". Peso de esta generacion: ",Modelo.pesoIn)
+        
+
